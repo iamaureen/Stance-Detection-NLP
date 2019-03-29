897d24f2e238b6b53a0a944ee7470527bd9642ff
iamaureen <ishrat.ahmed@asu.edu>
2019-03-29 17:33:20 -0400

    initial lstm for claim data

---
+++ claim_lstm_model.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from numpy import asarray
+from numpy import zeros
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+
+#filename
+filename = "data/emergent.csv"
+
+
+fields = ['claim','claim_label','body','page_headline','page_position']
+
+df = pd.read_csv(filename, usecols=fields)
+
+#todo remove duplicate claim rows and corresponding page position too
+claim_content = df['claim'];
+print(len(claim_content));
+
+#labels
+page_position = df['page_position'];
+print(len(page_position));
+
+df.loc[df['page_position'] == 'for', 'page_position'] = 1
+df.loc[df['page_position'] == 'against', 'page_position'] = 2
+df.loc[df['page_position'] == 'observing', 'page_position'] = 3
+df.loc[df['page_position'] == 'ignoring', 'page_position'] = 4
+df.loc[df['page_position'] == 'null', 'page_position'] = 5
+
+
+page_position_list = df.page_position.unique()
+print(page_position_list)
+
+# prepare tokenizer
+t = Tokenizer()
+t.fit_on_texts(claim_content)
+vocab_size = len(t.word_index) + 1
+
+# integer encode the documents
+encoded_docs = t.texts_to_sequences(claim_content)
+# print(encoded_docs)
+
+# https://stackoverflow.com/questions/34757703/how-to-get-the-longest-length-string-integer-float-from-a-pandas-column-when-the?rq=1
+# get the max length of the claim column
+claim_field_length = df.claim.astype(str).map(len)
+# print (type(df.loc[claim_field_length.idxmax(), 'claim']))
+# print (type(df.loc[claim_field_length.idxmin(), 'claim']))
+
+max_length_str = df.loc[claim_field_length.idxmax(), 'claim'];
+max_length = len(max_length_str.split());
+
+padded_claim_content = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+print(padded_claim_content)
+# load the whole embedding into memory
+embeddings_index = dict()
+f = open('/Users/iamaureen/Documents/glove.6B/glove.6B.100d.txt')
+for line in f:
+	values = line.split()
+	word = values[0]
+	coefs = asarray(values[1:], dtype='float32')
+	embeddings_index[word] = coefs
+f.close()
+print('Loaded %s word vectors.' % len(embeddings_index))
+# create a weight matrix for words in training docs
+embedding_matrix = zeros((vocab_size, 100))
+for word, i in t.word_index.items():
+	embedding_vector = embeddings_index.get(word)
+	if embedding_vector is not None:
+		embedding_matrix[i] = embedding_vector
+
+## create the LSTM model
+model_lstm = Sequential()
+model_lstm.add(Embedding(vocab_size, 100, input_length=24, weights=[embedding_matrix], trainable=False))
+# model_lstm.add(Dropout(0.2))
+# model_lstm.add(Conv1D(64, 5, activation='relu'))
+# model_lstm.add(MaxPooling1D(pool_size=4))
+model_lstm.add(LSTM(100))
+model_lstm.add(Dense(1, activation='sigmoid'))
+model_lstm.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+## Fit train data
+model_lstm.fit(padded_claim_content, page_position, validation_split=0.4, epochs = 3)
+
+# evaluate the model
+loss, accuracy = model_lstm.evaluate(padded_claim_content, page_position, verbose=0)
+print('Accuracy: %f' % (accuracy*100))
